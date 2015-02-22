11477fb71af935e671ccaed5f900a97e2bc49e4c
Payam Yavari <payam@yavtech.se>
2015-02-22 20:20:16 +0100

    Lesson 2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -9,6 +9,7 @@ namespace WinTail
     /// </summary>
     class ConsoleReaderActor : UntypedActor
     {
+        public const string StartCommand = "start";
         public const string ExitCommand = "exit";
         private ActorRef _consoleWriterActor;
 
@@ -19,18 +20,61 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && read.ToLowerInvariant().Equals(ExitCommand))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
+                DoPrintInstructions();
+                Self.Tell(new Messages.ContinueProcessing());
                 return;
             }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
+            }
 
-            _consoleWriterActor.Tell(read);
-            Self.Tell("continue");
+            GetAndValidateInput();
         }
 
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                // signal that the user needs to supply an input, as previously
+                // received input was blank
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (message.Equals(ExitCommand))
+            {
+                // shut down the entire actor system (allows the process to exit)
+                Context.System.Shutdown();
+            }
+            else
+            {
+                if (IsValid(message))
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+
+                    // continue reading messages from console
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+        }
+
+        private static bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
     }
 }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -9,8 +9,6 @@ namespace WinTail
 
         static void Main(string[] args)
         {
-            PrintInstructions();
-
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
             var consoleWriterActor = MyActorSystem.ActorOf(
@@ -21,24 +19,9 @@ namespace WinTail
                     Props.Create(() => new ConsoleReaderActor(consoleWriterActor)),
                     "consoleReaderActor");
 
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
             MyActorSystem.AwaitTermination();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
 }
